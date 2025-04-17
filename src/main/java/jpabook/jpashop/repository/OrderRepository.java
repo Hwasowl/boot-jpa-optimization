100c4ecb56a629c11b11e8070289de1943df2762
Hwasowl <sound2339@naver.com>
2025-04-17 23:15:09 +0900

    feat: v3 fetch join optimization

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -102,11 +102,12 @@ public class OrderRepository {
     }
 
     public List<Order> findAllWithMemberDelivery() {
+        // fetch join.
         return em.createQuery(
                 "select o from Order o" +
                         " join fetch o.member m" +
                         " join fetch o.delivery d", Order.class)
-                .getResultList();
+            .getResultList();
     }
 
     public List<Order> findAllWithItem() {
